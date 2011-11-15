0307e360cd89000526ce14d7973d1d8334df61f9
Bas Westerbaan <bas@westerbaan.name>
2011-11-15 16:04:50 +0100

    Change codestyle
    - 4 spaces instead of 8
    - add modelines
    
    Signed-off-by: Bas Westerbaan <bas@westerbaan.name>

--- urls.py
+++ urls.py
@@ -1,3 +1,4 @@
+# vim: et:sta:bs=2:sw=4:
 from django.conf.urls.defaults import *
 from django.conf import settings
 
--- views.py
+++ views.py
@@ -1,3 +1,4 @@
+# vim: et:sta:bs=2:sw=4:
 from django.views.decorators.cache import cache_page
 from django.core.servers.basehttp import FileWrapper
 from django.core.exceptions import PermissionDenied
@@ -18,70 +19,70 @@ import json
 import os
 
 def direct_to_folder(request, root, path):
-        """ Serves files under <root> statically.
-        
-        Used to serve /media. """
-        root = os.path.abspath(root)
-        p = os.path.abspath(os.path.join(root, path))
-        if not p.startswith(root):
-                raise PermissionDenied, "Going outside of the root"
-        if not os.path.exists(p):
-                raise Http404
-        if not os.path.isfile(p):
-                raise PermissionDenied, "This is not a file"
-        if os.stat(p).st_mode & 4 != 4:
-                raise PermissionDenied, "Not world readable"
-        return HttpResponse(FileWrapper(open(p)),
-                        mimetype=mimetypes.guess_type(p)[0])
+    """ Serves files under <root> statically.
+
+    Used to serve /media. """
+    root = os.path.abspath(root)
+    p = os.path.abspath(os.path.join(root, path))
+    if not p.startswith(root):
+        raise PermissionDenied, "Going outside of the root"
+    if not os.path.exists(p):
+        raise Http404
+    if not os.path.isfile(p):
+        raise PermissionDenied, "This is not a file"
+    if os.stat(p).st_mode & 4 != 4:
+        raise PermissionDenied, "Not world readable"
+    return HttpResponse(FileWrapper(open(p)),
+            mimetype=mimetypes.guess_type(p)[0])
 
 def home(request):
-        """ Main view """
-        return render_to_response('home.html',
-                        context_instance=RequestContext(request))
+    """ Main view """
+    return render_to_response('home.html',
+            context_instance=RequestContext(request))
 
 @cache_page(5)
 def api(request):
-        """ Returns the occupation """
-        l = logging.getLogger(__name__ + '.api')
-        ret = {'format': 1,
-               'generated': str(datetime.datetime.now())}
-        # scrape occupation from ru.nl
-        occup = scrape_bezetting()
-        # fetch schedule from api.ruuster.nl
-        schedule = cache.get('schedule')
-        if schedule is None: # cache miss
-                try:
-                        room_ids = cache.get('ruuster-roomids')
-                        if room_ids is None: # cache miss
-                                room_ids = fetch_room_ids(occup.keys())
-                                cache.set('ruuster-roomids', room_ids, 60*60)
-                        inst_id = cache.get('ruuster-instid')
-                        if inst_id is None: # cache miss
-                                inst_id = fetch_inst_id()
-                                cache.set('ruuster-instid', inst_id, 60*60)
-                        schedule = fetch_schedule(room_ids, inst_id)
-                        cache.set('schedule', schedule, 60)
-                except urllib2.HTTPError:
-                        # We do not want our service to be down, when
-                        # ruuster is down.
-                        # TODO be agnostic of ruuster.py's usage of urllib2
-                        l.exception("HTTPError from ruuster")
-                        schedule = {}
-        # combine
-        rooms = ret['rooms'] = []
-        for room_name in occup:
-                res = schedule.get(room_name)
-                if res is not None:
-                        res = {'starttime': res[0].strftime('%H:%M'),
-                               'endtime': res[1].strftime('%H:%M'),
-                               'now': res[0] <= datetime.datetime.now().time(),
-                               'name': res[2]}
-                rooms.append({
-                        'name': room_name,
-                        'free': occup[room_name][0],
-                        'capacity': occup[room_name][0] + occup[room_name][1],
-                        'reservation': res})
-        # sort rooms by name
-        rooms.sort(cmp=lambda x,y: cmp(x['name'], y['name']))
-        return HttpResponse(json.dumps(ret),
-                        mimetype='application/json')
+    """ Returns the occupation """
+    l = logging.getLogger(__name__ + '.api')
+    ret = {'format': 1,
+           'generated': str(datetime.datetime.now())}
+    # scrape occupation from ru.nl
+    occup = scrape_bezetting()
+    # fetch schedule from api.ruuster.nl
+    schedule = cache.get('schedule')
+    if schedule is None: # cache miss
+        try:
+            room_ids = cache.get('ruuster-roomids')
+            if room_ids is None: # cache miss
+                room_ids = fetch_room_ids(occup.keys())
+                cache.set('ruuster-roomids', room_ids, 60*60)
+            inst_id = cache.get('ruuster-instid')
+            if inst_id is None: # cache miss
+                inst_id = fetch_inst_id()
+                cache.set('ruuster-instid', inst_id, 60*60)
+            schedule = fetch_schedule(room_ids, inst_id)
+            cache.set('schedule', schedule, 60)
+        except urllib2.HTTPError:
+            # We do not want our service to be down, when
+            # ruuster is down.
+            # TODO be agnostic of ruuster.py's usage of urllib2
+            l.exception("HTTPError from ruuster")
+            schedule = {}
+    # combine
+    rooms = ret['rooms'] = []
+    for room_name in occup:
+        res = schedule.get(room_name)
+        if res is not None:
+            res = {'starttime': res[0].strftime('%H:%M'),
+                   'endtime': res[1].strftime('%H:%M'),
+                   'now': res[0] <= datetime.datetime.now().time(),
+                   'name': res[2]}
+        rooms.append({
+            'name': room_name,
+            'free': occup[room_name][0],
+            'capacity': occup[room_name][0] + occup[room_name][1],
+            'reservation': res})
+    # sort rooms by name
+    rooms.sort(cmp=lambda x,y: cmp(x['name'], y['name']))
+    return HttpResponse(json.dumps(ret),
+            mimetype='application/json')
